b2b002950cab29e7a54f2a82754c8180de51bda9
yamee-dev <ryo.eiyama@icloud.com>
2020-06-06 20:11:34 +0900

    mysqlに変更/API作成

--- resources/js/components/User.js
+++ resources/js/components/User.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function User() {
-    const users = [
-        {
-            id: 1,
-            name: 'John',
-            email: 'john@example.com'
-        },
-        {
-            id: 2,
-            name: 'Kevin',
-            email: 'kevin@test.com'
-        },
-        {
-            id: 3,
-            name: 'Joshua',
-            email: 'joshua@example.com'
-        }
-    ]
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers()
+    },[]);
+
+    const getUsers = async () => {
+        const response = await axios.get('/api/user');
+        setUsers(response.data.users);
+    };
 
 
     return (
         <div>
             <h1>Userページ</h1>
             <ul>
-                {users.map((user) => <li key={user.id} >
+                {users.map((user) =>
+                    <li key={user.id} >
                     {user.name}
+                    <Link to={`/user/${user.id}`}>
+                        詳細
+                    </Link>
                 </li> )}
             </ul>
         </div>
